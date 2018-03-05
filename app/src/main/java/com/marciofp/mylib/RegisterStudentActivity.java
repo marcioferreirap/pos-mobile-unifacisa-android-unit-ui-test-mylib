9fa5d950b8bd149b24b90b4ca726ce25996aeff3
Márcio Pereira <marcio.pereira@virtus.ufcg.edu.br>
2018-03-05 17:23:34 -0300

    Initial Commit

---
+++ app/src/main/java/com/marciofp/mylib/RegisterStudentActivity.java
@@ -0,0 +1,13 @@
+package com.marciofp.mylib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_student);
+    }
+}
